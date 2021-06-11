c5ebed1dcb5b62b030f08e865aa39dbdc73fd6b0
Victor Raposo <vwraposo@gmail.com>
2021-06-11 15:23:39 -0300

    Adds messages builder and button link

--- react/NotFoundBox.tsx
+++ react/NotFoundBox.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { Box, Button } from 'vtex.styleguide'
+import { Link } from 'vtex.render-runtime'
 
 import styles from './global.module.css'
+import { FormattedMessage } from 'react-intl'
+
+const ALL_APPS_PATH = '/all-apps'
 
 const NotFoundBox = () => {
   return (
 		<div className={styles.notFoundBox}>
 			<Box noPadding={true}>
         <div className="flex flex-column bg-white pa8">
-          <h2 className="flex center mid-gray b mb0">App unavailable for this currency</h2>
+          <h2 className="flex center mid-gray b mb0">
+            <FormattedMessage id="store/not-found-box-title" />
+          </h2>
           <p className="flex center gray">
-            Try changing currency or choose another similar app from the same category
+            <FormattedMessage id="store/not-found-box-subtitle" />
           </p>
           <div className="flex center pt4">
-            <Button>
-               Back to Store
-            </Button>
+            <Link to={ALL_APPS_PATH}>
+              <Button size="regular" variation="primary">
+                <FormattedMessage id="store/not-found-button" />
+              </Button>
+            </Link>
           </div>
         </div>
       </Box>
