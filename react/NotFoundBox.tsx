a84af336c34305be243b4903547bf8f6e2738aeb
Victor Raposo <vwraposo@gmail.com>
2021-06-11 11:00:09 -0300

    Not Found box components

--- react/NotFoundBox.tsx
+++ react/NotFoundBox.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { Box } from 'vtex.styleguide'
+import { Box, Button } from 'vtex.styleguide'
 
 import styles from './global.module.css'
 
 const NotFoundBox = () => {
   return (
 		<div className={styles.notFoundBox}>
-			<Box title="App unavailable for this currency">
-				<div className="mid-gray">
-					Test
+			<Box noPadding={true}>
+        <div className="flex flex-column bg-white pa8">
+          <h2 className="flex center mid-gray b mb0">App unavailable for this currency</h2>
+          <p className="flex center gray">
+            Try changing currency or choose another similar app from the same category
+          </p>
+          <div className="flex center pt4">
+            <Button>
+               Back to Store
+            </Button>
+          </div>
         </div>
-			</Box>
-		</div>
-	)
+      </Box>
+    </div>
+  )
 }
 
 export default NotFoundBox
